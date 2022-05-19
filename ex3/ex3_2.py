c414916511523f6644d6b2c0e1a4fcdce52ffb38
cylian <dopaminel@foxmail.com>
2022-05-19 14:28:22 +0000

    ex3

---
+++ ex3/ex3_2.py
@@ -0,0 +1,46 @@
+import numpy as np
+from ex3_1 import simplex
+
+
+def two_stages_simplex(A, b, c):
+    A_extended = np.concatenate([A, np.eye(len(b))], axis=1)
+    c_tmp = np.zeros((A_extended.shape[1], 1))
+    for i in range(1, len(b) + 1):
+        c_tmp[-i, 0] = 1
+
+    index = []
+    base = len(c)
+    for i in range(len(b)):
+        index.append(base)
+        base += 1
+    b, A, index = simplex(A_extended, b, c_tmp, index)
+    A = A[:, : -len(b)]
+    result, _, _ = simplex(A, b, c, index)
+    return result
+
+
+A = np.array(
+    [
+        [1, 1, 1, 0],
+        [5, 3, 0, -1],
+    ]
+)
+
+b = np.array(
+    [
+        [4],
+        [8],
+    ]
+)
+
+c = np.array(
+    [
+        [-3],
+        [-5],
+        [0],
+        [0],
+    ]
+)
+
+result = two_stages_simplex(A, b, c)
+print(result)
